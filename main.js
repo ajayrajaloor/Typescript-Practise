510d31e4fd1b082fc29bbd6168d9669ea26c1745
ajayraj1111 <121021527+ajayraj1111@users.noreply.github.com>
2023-08-16 22:48:04 +0530

    initial setup of typescript done

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// by importing or exporting statement typescript treats this file as a module instead of a script 
+var message = 'Welcome Back';
+console.log(message);
---
+++ main.ts
@@ -0,0 +1,4 @@
+export {}
+// by importing or exporting statement typescript treats this file as a module instead of a script 
+let message = 'Welcome Back'
+console.log(message);
