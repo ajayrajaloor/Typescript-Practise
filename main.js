1d1933f263c085a1fd462cc0b1bed7024bfcc4bd
ajayraj1111 <121021527+ajayraj1111@users.noreply.github.com>
2023-08-16 23:47:42 +0530

    tried writing the type of variables also tried ts in template strings

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 // by importing or exporting statement typescript treats this file as a module instead of a script 
-var message = 'Welcome Back';
+var message = 'Welcome';
 console.log(message);
+var isBeginner = true; //we need to initialize the type when it is declared
+var total = 1;
+var name = 'Ajayraj';
+// we can also write template strings in TS ↓
+var sentence = "My name is ".concat(name, "\nI am a Beginner in Typescript");
+console.log(sentence);
--- main.ts
+++ main.ts
@@ -1,4 +1,16 @@
 export {}
 // by importing or exporting statement typescript treats this file as a module instead of a script 
-let message = 'Welcome Back'
+let message = 'Welcome'
 console.log(message);
+
+
+
+let isBeginner : boolean = true; //we need to initialize the type when it is declared
+let total : number = 1;
+let name : string = 'Ajayraj';
+
+// we can also write template strings in TS ↓
+let sentence : string = `My name is ${name}
+I am a Beginner in Typescript`;
+
+console.log(sentence);
